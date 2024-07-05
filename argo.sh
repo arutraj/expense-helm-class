cd659a3d1cbb3a0d8cb4150568a4feaf26894c88
SVR Kalluru <raghuk.vit@gmail.com>
2024-07-05 08:09:40 +0530

    Expense Project Terraform

---
+++ argo.sh
@@ -0,0 +1,6 @@
+ARGO_URL=$(kubectl get svc argocd-server -n argocd| grep argocd-server | awk '{print $4}')
+ARGO_PASSWORD=$(kubectl get secrets -n argocd argocd-initial-admin-secret -o json  | jq '.data.password' | xargs | base64 --decode)
+
+argocd login $ARGO_URL --username admin --password $ARGO_PASSWORD --insecure
+
+argocd app create backend --repo https://github.com/raghudevopsb79/expense-helm --path dev/backend.yaml --dest-namespace default --dest-server https://kubernetes.default.svc --directory-recurse
