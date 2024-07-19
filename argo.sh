214316140b2c49ec66f6fe2e382f32e203bbfb79
SVR Kalluru <raghuk.vit@gmail.com>
2024-07-19 08:19:15 +0530

    Argo script update in pipeline

--- argo.sh
+++ argo.sh
@@ -8,6 +8,8 @@ argocd login $ARGO_URL --username admin --password $ARGO_PASSWORD --grpc-web
 argocd app list | grep "argocd/${2}"
 if [ $? -ne 0 ]; then
   argocd app create ${2} --repo https://github.com/raghudevopsb79/expense-helm --path . --dest-namespace default --dest-server https://kubernetes.default.svc --values ${1}/${2}.yaml --sync-policy auto --grpc-web --helm-set imageTag=$3
-  argocd app wait backend
+  argocd app wait ${2}
 fi
 
+argocd app set ${2} --parameter imageTag=$3
+argocd app wait ${2}
